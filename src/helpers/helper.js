5ac5b94aa6f1e9f93809073acba4813a1d9f864e
Serge Meyer <40215327+bele321@users.noreply.github.com>
2020-03-10 00:58:17 +0100

    test

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -32,7 +32,6 @@ const connectToHost = async (hosts) => {
     } 
     catch (error) {
       console.log(`${host.name} not running: ${error.name}`)
-      console.log(error)
     }
   })
   const results = await Promise.all(promises)
